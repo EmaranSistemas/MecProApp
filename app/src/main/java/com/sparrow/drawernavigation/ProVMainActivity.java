31315ab0e4713e0bd060355ca7cb34680df320d4
EmaranSistemas <sistemas.emaransac@gmail.com>
2023-06-23 16:55:31 -0500

    fhfhg

--- app/src/main/java/com/sparrow/drawernavigation/ProVMainActivity.java
+++ app/src/main/java/com/sparrow/drawernavigation/ProVMainActivity.java
@@ -43,7 +43,7 @@ import java.util.Map;
 public class ProVMainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView, recyclerView2;
-    private PfAdapter adapter, adapter2,adapter3,adapter4;
+    private PfAdapter adapter, adapter2;
 
     String[] distribuidores = {"Adriel","Francisco","Clara","Rodolfo","Juan Carlos","María","Carmen","Mirtha","Gerardo"};
     String[] categorias = {"Bodega","Minimarket","Supermercado","Especiería","Puesto de Mercado",
@@ -54,20 +54,17 @@ public class ProVMainActivity extends AppCompatActivity {
 
     String[] frescosarr = {"AJI PANCA FRESCO BATAN x 24 SBS","AJI AMARILLO FRESCO BATAN x24 SBS","AJO FRESCO BATAN x 24 SBS","CULANTRO FRESCO BATAN x 24 SBS"};
 
-
-
     String Distribuidor;
     String categoriasfinal;
-
     GpsTracker gpsTracker;
-    AutoCompleteTextView distribuidor;
-    AutoCompleteTextView categoria;
-    AutoCompleteTextView polvos_list;
-    AutoCompleteTextView frescos_list;
     ArrayAdapter<String> adapterdistribuidores;
     ArrayAdapter<String> adaptercategoria;
     ArrayAdapter<String> adapterpolvos;
     ArrayAdapter<String> adapterfrescos;
+
+    Spinner categoria_list;
+    Spinner polvos_list;
+    Spinner frescos_list;
 
 
 
@@ -90,13 +87,293 @@ public class ProVMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pro_vmain);
 
 
-        Spinner spinner = findViewById(R.id.distribuidor_txt);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, distribuidores);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        try { //Request Permission if not permitted
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        pro_frecos.clear();
+        pro_polvos.clear();
+
+
+        recyclerView = findViewById(R.id.mypolvos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new PfAdapter(this, pro_polvos);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView2 = findViewById(R.id.myfrecos);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(this));
+        adapter2 = new PfAdapter(this, pro_frecos);
+        recyclerView2.setAdapter(adapter2);
+
+
+        Spinner distribuidor = findViewById(R.id.distribuidor_txt);
+        adapterdistribuidores = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, distribuidores);
+        adapterdistribuidores.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        distribuidor.setAdapter(adapterdistribuidores);
 
 
 
+
+        txtCliente = findViewById(R.id.cliente_txt);
+        txtTelefono = findViewById(R.id.telefono_txt);
+        txtDireccion = findViewById(R.id.direccion_txt);
+        txtNombreComercial = findViewById(R.id.nombre_co);
+
+
+        categoria_list = findViewById(R.id.categoria_txt);
+        adaptercategoria = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categorias);
+        adaptercategoria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoria_list.setAdapter(adaptercategoria);
+        categoria_list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                categoriasfinal = item;
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        polvos_list = findViewById(R.id.polvos_txt);
+        adapterpolvos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, polvosarr);
+        adapterpolvos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        polvos_list.setAdapter(adapterpolvos);
+        polvos_list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                //polvosfinal = item;
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+
+
+/*
+       polvos_list = findViewById(R.id.polvos_txt);
+       adapterpolvos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, polvosarr);
+        adapterpolvos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        polvos_list.setAdapter(adapterpolvos);
+
+
+       frescos_list = findViewById(R.id.polvos_txt);
+       adapterfrescos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, frescosarr);
+        adapterfrescos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        frescos_list.setAdapter(adapterfrescos);
+
+        */
+
+
+        checkBoxExhibidor = findViewById(R.id.exhibidor);
+        checkBoxPop = findViewById(R.id.pop);
+
+
+
+
+        txtVentas = findViewById(R.id.ventas_txt);
+        txtObservaciones = findViewById(R.id.observaciones_txt);
+
+        btn_insert = findViewById(R.id.btn_guardar);
+        btn_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertData();
+            }
+        });
 
     }
+
+
+    private void insertData() {
+        try { //Request Permission if not permitted
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final String distribuidor = Distribuidor;
+        final String cliente = txtCliente.getText().toString().trim();//obligatorio
+        final String telefono = txtTelefono.getText().toString().trim();//opcional
+        final String direccion = txtDireccion.getText().toString().trim();//obligatorio
+        final String nombrecomercial = txtNombreComercial.getText().toString().trim();//opcional
+        final String categoria = categoriasfinal;
+
+
+        //final String polvos = polvos_list.getText().toString().trim();
+        //final String frescos = frescos_list.getText().toString().trim();
+
+        boolean isCheckedEx = checkBoxExhibidor.isChecked();
+        boolean isCheckedPop = checkBoxPop.isChecked();
+
+
+        final String ventas = txtVentas.getText().toString().trim();
+        final String observaciones = txtObservaciones.getText().toString().trim();
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("cargando...");
+
+        if(cliente.isEmpty()){
+            Toast.makeText(this, "Ingrese Cliente", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(direccion.isEmpty()){
+            Toast.makeText(this, "Ingrese Teléfono", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(categoria.isEmpty()){
+            Toast.makeText(this, "Ingrese Categoria", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(ventas.isEmpty()){
+            Toast.makeText(this, "Ingrese Ventas", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+/*
+            List<Frescos> lista = adapter.getPfList();
+            String[] arraypolvos = new String[lista.size()];
+            int i = 0;
+            for (Frescos f : lista) {
+                arraypolvos[i] = f.getName();
+                i++;
+            }
+
+
+            List<Frescos> lista2 = adapter2.getPfList();
+            String[] arraypolvos2 = new String[lista2.size()];
+            int i2 = 0;
+            for (Frescos f2 : lista2) {
+                arraypolvos2[i2] = f2.getName();
+                i2++;
+            }
+
+            JSONArray jsonArray = new JSONArray(Arrays.asList(arraypolvos));
+            JSONArray jsonArray2 = new JSONArray(Arrays.asList(arraypolvos2));
+
+
+            progressDialog.show();
+
+
+
+
+            StringRequest request = new StringRequest(Request.Method.POST, "https://emaransac.com/android/insertar_reporte_promotor.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if(response.equalsIgnoreCase("Se guardo correctamente.")){
+                                Toast.makeText(ProVMainActivity.this, "Se guardo correctamente.", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(ProVMainActivity.this, response, Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(ProVMainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String,String>();
+
+                    String valorCadena1 = String.valueOf(isCheckedEx);
+                    String valorCadena2 = String.valueOf(isCheckedPop);
+
+
+                    String a_lat = "0";
+                    String a_lon = "0";
+                    a_lat = getLocs(1);
+                    a_lon = getLocs(2);
+
+
+                    params.put("distribuidor",distribuidor);
+                    params.put("cliente",cliente);
+                    params.put("telefono",telefono);
+                    params.put("direccion",direccion);
+                    params.put("nombrecomercial",nombrecomercial);
+                    params.put("categoriasfinal",categoriasfinal);
+                    params.put("polvosarr", jsonArray.toString());
+                    params.put("frescosarr", jsonArray2.toString());
+                    params.put("isCheckedEx",valorCadena1);
+                    params.put("isCheckedPop",valorCadena2);
+                    params.put("ventas",ventas);
+                    params.put("observaciones",observaciones);
+                    params.put("longitud",a_lon);
+                    params.put("latitud",a_lat);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(ProVMainActivity.this);
+            requestQueue.add(request);
+
+            */
+
+            int a;
+        }
+
+        Log.d("TAG--------------------", "distrinuidor: " + distribuidor +
+                "  cliente: " + cliente +
+                "  Telefono: " + telefono +
+                "  Direccion: " + direccion +
+                "  NombreCom: " + nombrecomercial +
+                "  Categoria: " + categoriasfinal +
+                //"  Polvos: " + polvos +
+                //"  Frescos: " + frescos +
+                "  Exhibidor: " + isCheckedEx +
+                "  Pop: " + isCheckedPop +
+                "  Ventas" + ventas +
+                "  Observaciones: " + observaciones+
+                "  Long: " + ventas +
+                "  Latitud: " + observaciones
+        );
+    }
+
+    public String getLocs(int ID) { //Get Current Lat and Lon 1=lat, 2=lon
+        String asd_lat = "";
+        String asd_lon = "";
+        gpsTracker = new GpsTracker(ProVMainActivity.this);
+        if (gpsTracker.canGetLocation()) {
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            asd_lat = String.valueOf(latitude);
+            asd_lon = String.valueOf(longitude);
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+        if (ID == 1) {
+            return asd_lat;
+        } else if (ID == 2) {
+            return asd_lon;
+        } else {
+            return "0";
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+
 }
