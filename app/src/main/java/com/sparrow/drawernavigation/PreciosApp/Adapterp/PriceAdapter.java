fe89d1c15a8f2f04e4e92299eb072fcd5d491e15
EmaranSistemas <sistemas.emaransac@gmail.com>
2023-06-27 16:40:06 -0500

    falta el pulir el backent

---
+++ app/src/main/java/com/sparrow/drawernavigation/PreciosApp/Adapterp/PriceAdapter.java
@@ -0,0 +1,168 @@
+package com.sparrow.drawernavigation.PreciosApp.Adapterp;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.sparrow.drawernavigation.PreciosApp.Entity.prices;
+import com.sparrow.drawernavigation.PromvApp.Entities.Frescos;
+import com.sparrow.drawernavigation.R;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriceAdapter extends RecyclerView.Adapter<PriceAdapter.ViewHolder> {
+
+    private final Context context;
+    private List<prices> pfList;
+    private final ArrayList<prices> productoArrayList;
+    private itemClickListener itemClickListener;
+    private TextInputListener textInputListener; // Agregado el campo TextInputListener
+
+    public PriceAdapter(Context context, ArrayList<prices> pricesArrayList, TextInputListener textInputListener) {
+        this.context = context;
+        this.productoArrayList = pricesArrayList;
+        this.textInputListener = textInputListener; // Asignado el TextInputListener
+        this.pfList = pricesArrayList;
+    }
+
+    public List<prices> getPfList() {
+        return pfList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.price_report, parent, false);
+        return new ViewHolder(view, textInputListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        prices pri = productoArrayList.get(position);
+
+        holder.txtid.setText(pri.getId());
+        holder.txtProduct.setText(pri.getProducto());
+        holder.txtFrasco.setText(pri.getFrasco());
+        holder.txtGramaje.setText(pri.getGramaje());
+        holder.txtPrecio.setText(pri.getPrecio());
+
+
+        holder.txtPrecio.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    holder.txtPrecio.setText("");
+                }
+            }
+        });
+
+        holder.txtGramaje.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    holder.txtGramaje.setText("");
+                }
+            }
+        });
+
+
+        holder.txtFrasco.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    holder.txtFrasco.setText("");
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return productoArrayList.size();
+    }
+
+    public void removeItem(int position) {
+        productoArrayList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView txtid;
+        TextView txtProduct;
+        TextView txtFrasco;
+        TextView txtGramaje;
+        TextView txtPrecio;
+        Button button;
+        itemClickListener itemClickListener;
+        TextInputListener textInputListener;
+
+        public ViewHolder(@NonNull View itemView,  TextInputListener textInputListener) {
+            super(itemView);
+
+            txtid = itemView.findViewById(R.id.txt_id);
+            txtProduct = itemView.findViewById(R.id.txt_producto);
+            txtFrasco = itemView.findViewById(R.id.txt_frasco);
+            txtGramaje = itemView.findViewById(R.id.txt_gramaje);
+            txtPrecio = itemView.findViewById(R.id.txt_precio);
+
+            button = itemView.findViewById(R.id.button);
+            this.textInputListener = textInputListener;
+
+            //button.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            String id = txtid.getText().toString();
+            String producto = txtProduct.getText().toString();
+            String frasco = txtFrasco.getText().toString();
+            String gramaje = txtGramaje.toString();
+            String precio = txtGramaje.getText().toString();
+
+
+            boolean isInternetAvailable = isInternetAvailable(); // Verificar disponibilidad de internet
+            boolean isGpsEnabled = isGpsEnabled(); // Verificar si el GPS está activado
+
+            if (isInternetAvailable && isGpsEnabled) {
+                    itemClickListener.onItemClick(getAdapterPosition());
+                    textInputListener.onTextInputClicked(id,producto,frasco, gramaje, precio);
+                    PriceAdapter.this.removeItem(getAdapterPosition());
+            } else {
+                if (!isInternetAvailable) {
+                    Toast.makeText(context, "No hay conexión a internet", Toast.LENGTH_SHORT).show();
+                }
+                if (!isGpsEnabled) {
+                    Toast.makeText(context, "El GPS no está activado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    public interface itemClickListener {
+        void onItemClick(int position);
+    }
+
+    public interface TextInputListener {
+        void onTextInputClicked(String id,String producto, String frasco, String gramaeje, String precio);
+    }
+
+    private boolean isInternetAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+        return networkCapabilities != null && (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR));
+    }
+
+    private boolean isGpsEnabled() {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+}
--- app/src/main/java/com/sparrow/drawernavigation/PreciosApp/ReportPriceActivity.java
+++ app/src/main/java/com/sparrow/drawernavigation/PreciosApp/ReportPriceActivity.java
@@ -1,16 +1,360 @@
 package com.sparrow.drawernavigation.PreciosApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 
-import com.sparrow.drawernavigation.R;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ReportPriceActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.sparrow.drawernavigation.PreciosApp.Adapterp.PriceAdapter;
+import com.sparrow.drawernavigation.PreciosApp.Entity.prices;
+import com.sparrow.drawernavigation.PromvApp.Entities.Frescos;
+import com.sparrow.drawernavigation.R;
+import com.sparrow.drawernavigation.Ubication.GpsTracker;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReportPriceActivity extends AppCompatActivity  implements PriceAdapter.TextInputListener {
+
+    private RecyclerView recyclerView;
+
+    private PriceAdapter adapter;
+    private prices pri;
+    GpsTracker gpsTracker;
+
+    public static ArrayList<prices> pricesArrayList = new ArrayList<>();
+
+    String url ="https://emaransac.com/android/productos_retail.php";
+    String url2 ="https://emaransac.com/android/productos_horeca.php";
+    String url3 ="https://emaransac.com/android/productos_tradicional.php";
+    String link;
+    String catprod;
+
+    EditText zona,marca,observaciones;
+
+
+    String[] catproducto = {"--cat-prod--","Tradicional","Horeca","Retail"};
+    ArrayAdapter<String> catproadapter;
+    Spinner catlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_price);
+
+
+        catlist = findViewById(R.id.catprod_txt);
+        catproadapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, catproducto);
+        catproadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        catlist.setAdapter(catproadapter);
+        catlist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+
+                if (position != 0) { // Cambia 0 por el índice del elemento vacío en el Spinner
+                    catprod = item;
+                    if(catprod.equals("Horeca")){
+                        link = url2;
+                    } else if (catprod.equals("Tradicional")) {
+                        link = url3;
+                    }else{
+                        link = url;
+                        catprod = "Retail";
+                    }
+                    Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(),"Ingrese Categoría",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        zona = findViewById(R.id.cliente_txt);
+        marca = findViewById(R.id.brand_txt);
+        observaciones = findViewById(R.id.txt_observaciones);
+
+
+        Button btnAgregar = findViewById(R.id.btn_agregar);
+        final int[] clickCount = {0};
+
+
+            btnAgregar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickCount[0]++;
+
+                    if (clickCount[0] == 1) {
+
+                        final String zonadistrito = zona.getText().toString().trim();//obligatorio
+                        final String marca_ = marca.getText().toString().trim();//opcional
+                        final String categoria_prod = catprod;
+                    /*
+                    pricesArrayList.add(new prices("001", "Ajo", "Marca1", "Frasco1", "100g", "10.99"));
+                    pricesArrayList.add(new prices("002", "AJI AMARILLO", "Marca2", "Frasco2", "200g", "5.99"));
+                    */
+
+                        if (marca_.isEmpty()) {
+                            Toast.makeText(ReportPriceActivity.this, "Ingrese Marca", Toast.LENGTH_SHORT).show();
+                            clickCount[0] = 0;
+                            return;
+                        } else if (categoria_prod.isEmpty()) {
+                            Toast.makeText(ReportPriceActivity.this, "Ingrese Categoria producto", Toast.LENGTH_SHORT).show();
+                            clickCount[0] = 0;
+                            return;
+                        } else {
+                            // Cambiar el color de fondo del botón y el texto en el primer clic
+                            btnAgregar.setBackgroundColor(Color.parseColor("#FF01579B"));
+                            btnAgregar.setText("GUARDAR");
+
+                            retrieveData(url);
+                            adapter.notifyDataSetChanged();
+                            Toast.makeText(ReportPriceActivity.this, "llenamos el array", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    } else if (clickCount[0] == 2) {
+                        // Restaurar el color de fondo y el texto original en el segundo clic
+                        btnAgregar.setBackgroundColor(Color.parseColor("#FF4CAF50"));
+                        btnAgregar.setText("AGREGAR");
+                        clickCount[0] = 0; // Reiniciar el contador de clics
+
+                        pricesArrayList.clear();
+                        adapter.notifyDataSetChanged();
+                        EditText marca = findViewById(R.id.brand_txt);
+                        marca.setText("");
+
+                        EditText obs = findViewById(R.id.txt_observaciones);
+                        obs.setText("");
+                        insertData();
+
+                        Toast.makeText(ReportPriceActivity.this, "Se guardan los datos", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        recyclerView = findViewById(R.id.recycler_price_view);
+        adapter = new PriceAdapter(this,pricesArrayList,this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
+
+    @Override
+    public void onTextInputClicked(String id, String producto, String frasco, String grameje, String precio) {
+        Log.d("------------------TAG PRICE--------------------",id + " -- " + producto + " -- " + marca + " -- " + frasco + " -- " + grameje + " -- " + precio);
+    }
+
+    public void retrieveData(String url){
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        pricesArrayList.clear();
+                        try{
+
+                            JSONObject jsonObject = new JSONObject(response);
+                            String exito = jsonObject.getString("exito");
+                            JSONArray jsonArray = jsonObject.getJSONArray("datos");
+                            if(exito.equals("1")){
+                                for(int i=0;i<jsonArray.length();i++){
+                                    JSONObject object = jsonArray.getJSONObject(i);
+                                    String id = object.getString("id");
+                                    String nombre = object.getString("nombre");
+                                    pri = new prices(id,nombre,"","","","");
+                                    pricesArrayList.add(pri);
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ReportPriceActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+
+
+
+    private void insertData() {
+        try { //Request Permission if not permitted
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final String zonadistrito = zona.getText().toString().trim();//obligatorio
+        final String marca_ = marca.getText().toString().trim();//opcional
+        final String categoria_prod = catprod;
+        final String observacines_ = observaciones.getText().toString().trim();
+
+
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("cargando...");
+
+        if(zonadistrito.isEmpty()){
+            Toast.makeText(this, "Ingrese Zona/Distrito", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(marca_.isEmpty()){
+            Toast.makeText(this, "Ingrese Marca", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(categoria_prod.isEmpty()){
+            Toast.makeText(this, "Ingrese Categoria producto", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+
+
+
+            List<prices> lista = adapter.getPfList();
+            String[] arraypolvos = new String[lista.size()];
+            int i = 0;
+            for (prices f : lista) {
+                arraypolvos[i] = f.getItem();
+                i++;
+            }
+
+
+
+
+            JSONArray jsonArray = new JSONArray(Arrays.asList(arraypolvos));
+            Log.d("----------NUESTRO ARRAY--------", jsonArray.toString());
+
+            progressDialog.show();
+
+ /*
+
+
+            StringRequest request = new StringRequest(Request.Method.POST, "https://emaransac.com/android/insertar_reporte_promotor.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if(response.equalsIgnoreCase("Se guardo correctamente.")){
+                                Toast.makeText(ReportPriceActivity.this, "Se guardo correctamente.", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(ReportPriceActivity.this, response, Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(ReportPriceActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+            ){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String,String>();
+
+
+                    String a_lat = "0";
+                    String a_lon = "0";
+                    a_lat = getLocs(1);
+                    a_lon = getLocs(2);
+
+
+
+
+                    params.put("zona",zonadistrito);
+                    params.put("catprod",categoria_prod);
+                    params.put("marca",marca_);
+                    params.put("list_detalle",jsonArray.toString());
+                    params.put("observaciones",observacines_);
+                    params.put("longitud",a_lon);
+                    params.put("latitud",a_lat);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(ReportPriceActivity.this);
+            requestQueue.add(request);
+
+            requestQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
+                @Override
+                public void onRequestFinished(Request<Object> request) {
+                    // Redirigir a PromotorActivity después de la inserción exitosa
+                    Intent intent = new Intent(ReportPriceActivity.this, PromotorActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+            */
+            Log.d("---REPORTE--DE--PRECIOS---", zonadistrito + "  " + categoria_prod + " "+marca+" "+observacines_+" ");
+        }
+
+
+
+        Log.d("---REPORTE--DE--PRECIOS---", zonadistrito + "  " + categoria_prod + " "+marca+" "+observacines_+" ");
+    }
+
+    public String getLocs(int ID) { //Get Current Lat and Lon 1=lat, 2=lon
+        String asd_lat = "";
+        String asd_lon = "";
+        gpsTracker = new GpsTracker(ReportPriceActivity.this);
+        if (gpsTracker.canGetLocation()) {
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            asd_lat = String.valueOf(latitude);
+            asd_lon = String.valueOf(longitude);
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+        if (ID == 1) {
+            return asd_lat;
+        } else if (ID == 2) {
+            return asd_lon;
+        } else {
+            return "0";
+        }
+    }
+
 }
